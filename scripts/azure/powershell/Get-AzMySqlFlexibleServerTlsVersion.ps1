8efd42a9469a2f0be5ead8e63063fb8df22ca095
Hélder Pinto <helder.pinto@microsoft.com>
2025-06-06 16:50:11 +0100

    Added Azure DB for MySQL retirements

---
+++ scripts/azure/powershell/Get-AzMySqlFlexibleServerTlsVersion.ps1
@@ -0,0 +1,76 @@
+param(
+    [Parameter(Mandatory = $false)] 
+    [String] $AzureEnvironment = "AzureCloud",
+
+    [Parameter(Mandatory = $false)] 
+    [String] $ResourceId
+)
+
+$ErrorActionPreference = "Stop"
+
+$ctx = Get-AzContext
+if (-not($ctx)) {
+    Connect-AzAccount -Environment $AzureEnvironment
+    $ctx = Get-AzContext
+}
+else {
+    if ($ctx.Environment.Name -ne $AzureEnvironment) {
+        Disconnect-AzAccount -ContextName $ctx.Name
+        Connect-AzAccount -Environment $AzureEnvironment
+        $ctx = Get-AzContext
+    }
+}
+
+if ($ResourceId)
+{
+    $resourceFilter = " and id=~'$ResourceId'"
+}
+
+$ARGPageSize = 1000
+
+$subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" } | ForEach-Object { "$($_.Id)"}
+
+$argQuery = @"
+resources 
+| where type =~ 'microsoft.dbformysql/flexibleservers'$resourceFilter
+| project id, name, resourceGroup, subscriptionId, location
+| order by id asc
+"@
+
+$servers = (Search-AzGraph -Query $argQuery -First $ARGPageSize -Subscription $subscriptions).data
+
+Write-Output "Found $($servers.Count) Azure Database for MySQL flexible servers."
+
+$serversDetails = @()
+
+foreach ($server in $servers) {
+
+    if ($server.subscriptionId -ne $ctx.Subscription.Id)
+    {
+        $ctx = Set-AzContext -SubscriptionId $server.subscriptionId | Out-Null
+    }
+
+    $serverTlsConfiguration = Get-AzMySqlFlexibleServerConfiguration -Name "tls_version" `
+        -ResourceGroupName $server.resourceGroup -ServerName $server.name -SubscriptionId $server.subscriptionId
+
+    $foregroundColor = "Green"
+    if ($serverTlsConfiguration.Value -like "*TLSv1,*" -or $serverTlsConfiguration.Value -like "*TLSv1.1*")
+    {
+        $foregroundColor = "Yellow"
+    }
+    Write-Host "Server: $($server.name) - TLS Version: $($serverTlsConfiguration.Value)" -ForegroundColor $foregroundColor
+
+    $serverDetails = New-Object PSObject -Property @{
+        ResourceId = $server.id
+        ServerName = $server.name
+        ResourceGroup = $server.resourceGroup
+        SubscriptionId = $server.subscriptionId
+        Region = $server.location
+        TLSVersions = $serverTlsConfiguration.Value
+    }
+    $serversDetails += $serverDetails    
+}
+
+$serversDetails | Export-Csv -Path "azuredb-mysql-tls-versions.csv" -NoTypeInformation
+
+Write-Output "Exported Azure Database for MySQL flexible servers TLS versions to azuredb-mysql-tls-versions.csv"
