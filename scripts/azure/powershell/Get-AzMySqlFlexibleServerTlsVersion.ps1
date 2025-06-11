f075c85ef6a37239567d56ba3df546bde20788b0
Hélder Pinto <helder.pinto@microsoft.com>
2025-06-11 21:59:05 +0100

    Added Batch Account query

--- scripts/azure/powershell/Get-AzMySqlFlexibleServerTlsVersion.ps1
+++ scripts/azure/powershell/Get-AzMySqlFlexibleServerTlsVersion.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    Retrieves the TLS versions used by Azure Database for MySQL flexible servers.
+
+.DESCRIPTION
+    This script connects to Azure, retrieves all Azure Database for MySQL flexible servers, and checks their TLS versions.
+    It outputs the results to a CSV file named "azuredb-mysql-tls-versions.csv". 
+    NOTE: this script works better when run in the Azure Cloud Shell.
+
+.PARAMETER AzureEnvironment
+    The Azure environment to connect to. Default is "AzureCloud".
+
+.PARAMETER ResourceId
+    The resource ID of a specific Azure Database for MySQL flexible server to check. If not provided, all servers will be checked.
+
+.EXAMPLE
+    ./Get-AzMySqlFlexibleServerTlsVersion
+    Retrieves the TLS versions for all Azure Database for MySQL flexible servers in the Azure Cloud environment.
+
+.EXAMPLE
+    ./Get-AzMySqlFlexibleServerTlsVersion -AzureEnvironment "AzureUSGovernment"
+    Retrieves the TLS versions for all Azure Database for MySQL flexible servers in the Azure US Government environment.
+
+.EXAMPLE
+    ./Get-AzMySqlFlexibleServerTlsVersion -ResourceId "/subscriptions/{subscription-id}/resourceGroups/{resource-group}/providers/Microsoft.DBforMySQL/flexibleServers/{server-name}"
+    Retrieves the TLS version for a specific Azure Database for MySQL flexible server identified by the provided resource ID.
+#>
+
 param(
     [Parameter(Mandatory = $false)] 
     [String] $AzureEnvironment = "AzureCloud",
